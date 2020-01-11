f1c3e9daa881796066530350dfa27850a9ae28a2
tnaz <tullah.nazari@gmail.com>
2020-01-11 13:42:05 -0600

    added user data to users table

---
+++ lib/providers/user_table_roles.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserTableRoles {
+  final String uid;
+  UserTableRoles({this.uid});
+
+  final CollectionReference usersCollection =
+      Firestore.instance.collection('users');
+
+  Future updateUserData(bool isAdmin) async {
+    return await usersCollection.document(uid).setData({'isAdmin': false});
+  }
+}
